2e7c3ffa6fe5cc6c20449791cd294378166717e9
Joao-Gontijo <jvgontijo166@gmail.com>
2022-04-05 14:32:52 -0300

    conta especial

---
+++ src/br/com/bytebank/especial/ContaEspecial.java
@@ -0,0 +1,16 @@
+package br.com.bytebank.especial;
+
+import br.com.bytebank.model.Conta;
+
+public class ContaEspecial extends Conta {
+
+	public ContaEspecial(int agencia, int numero) {
+		super(agencia, numero);
+	}
+
+	@Override
+	public void deposita(double valor) {
+		super.saldo += valor;
+	}
+
+}
