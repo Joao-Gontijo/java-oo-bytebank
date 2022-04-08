f79cdecd79b6f1a096b3d70000db57de5ce48dc7
Joao-Gontijo <jvgontijo166@gmail.com>
2022-04-07 22:51:24 -0300

    array de referencias

--- src/br/com/bytebank/testes/TestaArrayReferencias.java
+++ src/br/com/bytebank/testes/TestaArrayReferencias.java
@@ -1,18 +1,26 @@
 package br.com.bytebank.testes;
 
+import br.com.bytebank.model.Conta;
 import br.com.bytebank.model.ContaCorrente;
+import br.com.bytebank.model.ContaPoupanca;
 
 public class TestaArrayReferencias {
 	public static void main(String[] args) {
 		
-		ContaCorrente[] contas = new ContaCorrente[3];
+//		ContaCorrente[] contas = new ContaCorrente[3];
+		Conta[] contas = new Conta[3];
 		
 		ContaCorrente cc1 = new ContaCorrente(10, 20);
-		ContaCorrente cc2 = new ContaCorrente(30, 40);
+		ContaPoupanca cc2 = new ContaPoupanca(30, 40);
 		
 		contas[0] = cc1;
 		contas[1] = cc2;
 	
+
+		ContaCorrente ref = (ContaCorrente) contas[0];
+		
 		System.out.println(contas[0].getNumero());
+		System.out.println(contas[1].getAgencia());
+		System.out.println(ref.getNumero());
 	}
 }
