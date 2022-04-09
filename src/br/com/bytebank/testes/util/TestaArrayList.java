786920ed0ba829014ee148d17fc695beb0586b1b
Joao-Gontijo <jvgontijo166@gmail.com>
2022-04-09 17:58:00 -0300

    arraylist inicio

---
+++ src/br/com/bytebank/testes/util/TestaArrayList.java
@@ -0,0 +1,43 @@
+package br.com.bytebank.testes.util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import br.com.bytebank.especial.ContaEspecial;
+import br.com.bytebank.model.Conta;
+import br.com.bytebank.model.ContaCorrente;
+import br.com.bytebank.model.ContaPoupanca;
+
+public class TestaArrayList {
+	public static void main(String[] args) {
+		
+		ArrayList lista = new ArrayList();
+		
+		Conta ce = new ContaEspecial(10, 20);
+		Conta cc = new ContaCorrente(0, 0);
+		Conta cp = new ContaPoupanca(1, 1);
+		
+		lista.add(ce);
+		lista.add(cc);
+		lista.add(cp);
+		
+		Conta ref = (Conta) lista.get(1);
+		System.out.println(ref);
+		
+		System.out.println("Tamanho: " + lista.size());
+		lista.remove(0);
+		System.out.println("Tamanho: " + lista.size());
+		
+		for (int i = 0; i < lista.size(); i++) {
+			System.out.println(lista.get(i));
+		}
+		
+		System.out.println("-------");
+		
+		for (Object object : lista) {
+			Conta c = (Conta) object;
+			System.out.println("Número da conta: " + c.getNumero());
+		}
+		
+	}
+}
