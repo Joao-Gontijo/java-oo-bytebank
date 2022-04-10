eef2319fcdae8e64cc6dfbe379879acdb7af3dfb
Joao-Gontijo <jvgontijo166@gmail.com>
2022-04-10 11:15:46 -0300

    vector

--- src/br/com/bytebank/testes/util/TestaArrayList.java
+++ src/br/com/bytebank/testes/util/TestaArrayList.java
@@ -1,7 +1,11 @@
 package br.com.bytebank.testes.util;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
 
 import br.com.bytebank.especial.ContaEspecial;
 import br.com.bytebank.model.Conta;
@@ -11,7 +15,13 @@ import br.com.bytebank.model.ContaPoupanca;
 public class TestaArrayList {
 	public static void main(String[] args) {
 		
-		ArrayList<Conta> lista = new ArrayList<Conta>();
+		//ArrayList<Conta> lista = new ArrayList<Conta>();
+		//List<Conta> lista = new LinkedList<Conta>();
+		
+		//utilizar Collection aqui não funciona pois
+		//ele não é sequencial e não tem indice
+		//Collection<Conta> lista = new Vector<Conta>();
+		List<Conta> lista = new Vector<Conta>();
 		
 		Conta ce = new ContaEspecial(10, 20);
 		Conta cc = new ContaCorrente(0, 0);
