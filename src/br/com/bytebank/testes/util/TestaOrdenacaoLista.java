2b3b150799c97d4bd5ed0ce3a9c707d3c790e783
Joao-Gontijo <jvgontijo166@gmail.com>
2022-04-11 15:23:24 -0300

    ordenação de string

--- src/br/com/bytebank/testes/util/TestaOrdenacaoLista.java
+++ src/br/com/bytebank/testes/util/TestaOrdenacaoLista.java
@@ -4,23 +4,36 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import br.com.bytebank.model.Cliente;
 import br.com.bytebank.model.Conta;
 import br.com.bytebank.model.ContaCorrente;
 import br.com.bytebank.model.ContaPoupanca;
 
 public class TestaOrdenacaoLista {
 	public static void main(String[] args) {
-		Conta cc1 = new ContaCorrente(22, 22);
-		cc1.deposita(333);
-		
-		Conta cp1 = new ContaPoupanca(22, 55);
-		cp1.deposita(777);
-		
+		Conta cc1 = new ContaCorrente(22, 33);
+		Cliente clienteCC1 = new Cliente();
+		clienteCC1.setNome("Nico");
+		cc1.setTitular(clienteCC1);
+		cc1.deposita(333.0);
+
+		Conta cp1 = new ContaPoupanca(22, 44);
+		Cliente clienteCC2 = new Cliente();
+		clienteCC2.setNome("Gabriel");
+		cp1.setTitular(clienteCC2);
+		cc1.deposita(444.0);
+
 		Conta cc2 = new ContaCorrente(22, 11);
-		cc2.deposita(111);
-	
-		Conta cp2 = new ContaPoupanca(22, 44);
-		cp2.deposita(222);
+		Cliente clienteCC3 = new Cliente();
+		clienteCC3.setNome("João");
+		cc2.setTitular(clienteCC3);
+		cc2.deposita(111.0);
+
+		Conta cp2 = new ContaPoupanca(22, 22);
+		Cliente clienteCC4 = new Cliente();
+		clienteCC4.setNome("Ana");
+		cp2.setTitular(clienteCC4);
+		cp2.deposita(222.0);
 	
 		
 		List<Conta> lista = new ArrayList<Conta>();
@@ -29,19 +42,28 @@ public class TestaOrdenacaoLista {
 		lista.add(cc2);
 		lista.add(cp2);
 		
-		lista.sort(new NumeroDaContaComparator());
+//		lista.sort(new NumeroDaContaComparator());
+		lista.sort(new TitularDaContaComparator());
 		
 		for (Conta conta : lista) {
-			System.out.println(conta);
+			System.out.println("Titular: " + conta.getTitular().getNome() + " - " + conta);
 		}
 	}
 }
 
 class NumeroDaContaComparator implements Comparator<Conta>{
-
 	@Override
 	public int compare(Conta c1, Conta c2) {
 		return c1.getNumero() - c2.getNumero();
 	}
+}
+
+class TitularDaContaComparator implements Comparator<Conta>{
 	
+	@Override
+	public int compare(Conta c1, Conta c2) {
+		String nome1 = c1.getTitular().getNome();
+		String nome2 = c2.getTitular().getNome();
+		return nome1.compareTo(nome2);
+	}
 }
